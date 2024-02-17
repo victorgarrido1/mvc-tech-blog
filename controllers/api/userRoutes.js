2a82c46f16790175ad67b196bb2287f32c43bd92
victorgarrido1 <vcgarr94@gmail.com>
2024-02-16 22:33:40 -0700

    begining to add main handle bars template

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,6 +2,3 @@ const router = require('express').Router();
 const { User, Blog, Comments } = require('../models');
 
 
-router.get('/', (req, res) => {
-        User.
-})
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,24 @@
 const router = require('express').Router();
-const
-const withAuth = require('../utils/withAuth');
+const { User, Blog, Comments } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get("/", async (req, res) => {
+    try {
+        // Retrieve all blog posts along with the associated usernames
+        const postData = await Blog.findAll({
+            include: [{ model: User, attributes: ["username"] }]
+        });
+
+        // Send the data as a response
+        res.json(postData);
+    } catch (err) {
+        // Handle error that may occur
+        console.error("Error fetching the blog posts:", err);
+        res.status(500).json({ error: "Failed to retrieve blog posts" });
+    }
+
+    const post = postData.map((post) => post.get({ plain: true
+    }));
+});
+
+module.exports = router;
