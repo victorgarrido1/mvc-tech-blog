4063485c32d86bacac075aa95cb447664e5ccb60
victorgarrido1 <vcgarr94@gmail.com>
2024-02-19 21:36:55 -0700

    adding edit functuality to the js files

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,28 @@
 //signup request
 const techSignupFromHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.
+  const username = document.querySelector("#username").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("api/users/signup", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up");
+    }
+  }
+};
+
+//Event listener
+const techSignupForm = document.querySelector("#sign-up-from");
+if (techSignupForm) {
+  techSignupForm.addEventListener("submit", techSignupFromHandler);
 }
