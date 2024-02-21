5b199ada168a568c66600aeb05542dd8ece1ecfe
victorgarrido1 <vcgarr94@gmail.com>
2024-02-21 02:40:37 -0700

    important commit, please observe changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -25,51 +25,53 @@ router.get("/", async (req, res) => {
 });
 
 // Route to render individual post page
-router.get("/post/:id", withAuth, async (req, res) => {
+router.get('/post/:id', async (req, res) => {
   try {
-    // Find post by ID with associated username and comments with associated usernames
     const postData = await Blog.findByPk(req.params.id, {
       include: [
-        { model: User, attributes: ["username"] },
+        User,
         {
-          model: Comment,
-          include: [{ model: User, attributes: ["username"] }],
+          model: Comments,
+          include: [User],
         },
       ],
     });
-    // Convert post data to plain JavaScript object
-    const post = postData.get({ plain: true });
-    // Render post template with post data and login status
-    res.render("post", {
-      ...post,
-      logged_in: req.session.logged_in,
-    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+      router.get('/post/:id', async (req, res) => {
+        try {
+          const postData = await Blog.findByPk(req.params.id, {
+            include: [
+              User,
+              {
+                model: Comment,
+                include: [User],
+              },
+            ],
+          });
+      
+          if (postData) {
+            const post = postData.get({ plain: true });
+      
+            res.render('post', { post, loggedIn: req.session.logged_in });
+          } else {
+            res.status(404).end();
+          }
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      });
+      res.render('post', { post, loggedIn: req.session.logged_in });
+    } else {
+      res.status(404).end();
+    }
   } catch (err) {
-    // If there is an error, return 500 status code and error message
     res.status(500).json(err);
   }
 });
 
-//Route to render dashboard page with all posts by current user
-//Find all the posts by current user with their username
-router.get("/dashboard", withAuth, async (req, res) => {
-  try {
-    const postData = await Blog.findAll({
-      where: { user_id: req.session.user_id },
-      include: [{ model: User, attributes: ["username"] }],
-    });
-    //convert post data to JavaScript object
-    const posts = postData.map((blog) => blog.get({ plain: true }));
-
-    res.render("dashboard", {
-      blog,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
+//We need to create a route for the post//
 router.get("/t")
 
 
